99c5d4780f94c3ed8bf78d7bade7586c781ddc6d
ahmed allam <3lam.ahmedd@gmail.com>
2021-09-23 23:37:10 -0700

    My Weather App

---
+++ Shared/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  Shared
+//
+//  Created by AhmedAllam on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
