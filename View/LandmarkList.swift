224bfdd3b6f488e1eceb30d4715409c3a4c1b268
ahmed allam <3lam.ahmedd@gmail.com>
2021-09-30 15:46:22 -0700

    update

---
+++ View/LandmarkList.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkList.swift
+//  first
+//
+//  Created by AhmedAllam on 30/09/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks){ landmark in
+            LandmarkRow(landmark: landmark)
+            
+           
+            
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
